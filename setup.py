d159f427493019e7d155287e696b0ea4193b6d22
Tim Vink <vinktim@gmail.com>
2022-06-08 14:13:22 +0200

    bump to v1.6.4

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ utils_dep = ["optbinning>=0.8.0"]
 
 setup(
     name="skorecard",
-    version="1.6.3",
+    version="1.6.4",
     description="Tools for building scorecard models in python, with a sklearn-compatible API",
     long_description=long_description,
     long_description_content_type="text/markdown",
